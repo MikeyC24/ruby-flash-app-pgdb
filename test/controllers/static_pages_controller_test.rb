4641ae2aea6f0f6d74c31ae6f5f40b6abb46c565
mikeyc24 <mgchapman24@gmail.com>
2017-06-07 16:59:42 -0400

    done with inital layouts

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get root_path
+    assert_response :success
+    assert_select "title", "Ruby Flash Card App"
+  end
+
+  test "should get help" do
+    get help_path
+    assert_response :success
+    assert_select "title", "Help"
+  end
+
+  test "should get about page" do 
+    get about_path
+    assert_response :success
+    assert_select "title", "About"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact"
+  end
+
+end
