8f5fab7a1ef37b6267b8af27a3172303704bc412
mikeyc24 <mgchapman24@gmail.com>
2017-06-10 21:12:21 -0400

    fixed model var

--- app/models/method_name_list.rb
+++ app/models/method_name_list.rb
@@ -1,3 +1,4 @@
 class MethodNameList < ApplicationRecord
 	serialize :method_name_hash
+	has_many :method_posts
 end
--- db/migrate/20170611010951_create_method_posts.rb
+++ db/migrate/20170611010951_create_method_posts.rb
@@ -1,6 +1,7 @@
 class CreateMethodPosts < ActiveRecord::Migration[5.1]
   def change
     create_table :method_posts do |t|
+      t.text :content
       t.references :user, foreign_key: true
       t.references :method_name_list, foreign_key: true
 
